1a2a6b762c2f52b348a2d2861c84ee1c5129dd7c
9yamamo9 <yamamori929yuta@outlook.jp>
2018-04-25 22:53:31 +0900

    hoge

---
+++ menberUrls/hirate.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+
+)
+
+var (
+  baseUrl string
+  blogNumber string
+)
+
+baseUrl = "http://www.keyakizaka46.com/s/k46o/diary/detail/" + blogNumber + "?ima=0000&cd=member"
+
+// TODO blogNumberを変更する
